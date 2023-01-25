5a23e72a6d7d3e9cd1829f50f94e99f1ef8e2596
adrielripoll <adrielripoll@hotmail.com>
2023-01-25 13:21:32 -0300

    test: add listById method to InMemoryDb

--- test/repositories/inMemoryLocationRepository.ts
+++ test/repositories/inMemoryLocationRepository.ts
@@ -8,4 +8,8 @@ export class InMemoryLocationRepository implements LocationRepository {
     async save(location: Location) {
         this.location.push(location)
     }
+
+    async listById(freightMatchId: number): Promise<Location[]> {
+        return this.location.filter((item) => item.freightMatchId == freightMatchId)
+    }
 }
