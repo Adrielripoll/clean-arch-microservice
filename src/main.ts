2c2728a55f15229d265b85bca94ec9be4399bab6
adrielripoll <adrielripoll@hotmail.com>
2023-01-27 13:29:28 -0300

    feat: add microservices connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const kafka = app.get(KafkaConsumerService)
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafka
+  })
+  await app.startAllMicroservices()
   await app.listen(3000);
 }
 bootstrap();
