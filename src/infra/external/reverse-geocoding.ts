c28e7287b7fe0d49dc7cb73ef34bbb69e8a979ee
adrielripoll <adrielripoll@hotmail.com>
2023-01-27 16:40:22 -0300

    change reverse geocoding api

--- src/infra/external/reverse-geocoding.ts
+++ src/infra/external/reverse-geocoding.ts
@@ -4,14 +4,18 @@ import axios from 'axios'
 export async function reverseGeocoding(coordinates: Coordinates): Promise<string>{
     const { latitude, longitude } = coordinates
 
-    const url: string = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.GOOGLE_API_KEY}`
-    const request = await axios.get(url)
+    const url: string = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=jsonv2`
+    const { data } = await axios.get(url)
     
-    const address = request.data.results[0].formatted_address
-
-    if(!address){
+    if(!data){
         throw new Error("Location not found")
     }
 
-    return address
+    const { address } = data
+
+    const addressInfo = [ address.road, address.suburb, address.city, address.state, address.country, address.postcode ]
+
+    const formatedAddress = addressInfo.join(', ')
+
+    return formatedAddress
 }
