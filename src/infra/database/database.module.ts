6fbe94944c0833029a2e56b9b2fdc7469c5324bb
adrielripoll <adrielripoll@hotmail.com>
2023-01-27 12:15:36 -0300

    fix: change database url connection

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,11 +1,11 @@
-import { LocationRepository } from '@domain/repository/location-repository'
+import { LocationRepository } from '@application/repository/location-repository'
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { MongoLocationRepository } from './mongo/repositories/location.repository'
 import { models } from './mongo/models/models'
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/locations'), MongooseModule.forFeature(models)],
+    imports: [MongooseModule.forRoot('mongodb://192.168.1.6:27017', {user: 'root', pass: '1234', dbName: 'locations'}), MongooseModule.forFeature(models)],
     providers: [{
         provide: LocationRepository,
         useClass: MongoLocationRepository
